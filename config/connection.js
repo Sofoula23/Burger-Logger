afa290efd45ac8639c84bda7a8c31a19e088a54a
Sofia <sofimantas@gmail.com>
2021-02-08 20:49:18 -0500

    added connection to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootroot',
+    database: 'todoagain_db',
+  });
+};
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
